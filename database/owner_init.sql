6bdcb97cb8833a598f2bb32b2f734be013044376
leo <Pestov.962@gmail.com>
2024-12-21 20:02:45 +0300

    #add check to exist med_user in owner_init.sql

--- database/owner_init.sql
+++ database/owner_init.sql
@@ -3,6 +3,15 @@ CREATE USER med_procedures_owner WITH PASSWORD 'super';
 CREATE DATABASE med_procedures_owner OWNER med_procedures_owner;
 ALTER USER med_procedures_owner WITH CREATEDB CREATEROLE;
 
--- Назначаем med_procedures_owner администратором для med_user
-GRANT med_user TO med_procedures_owner WITH ADMIN OPTION;
+-- Проверяем, существует ли пользователь med_user
+DO $$
+BEGIN
+  IF role_exists('med_user') THEN
+    -- Назначаем med_procedures_owner администратором для med_user
+    GRANT med_user TO med_procedures_owner WITH ADMIN OPTION;
+    RAISE NOTICE 'med_procedures_owner назначен администратором для med_user';
+  ELSE
+    RAISE NOTICE 'Пользователь med_user не существует, пропускаем назначение администратора';
+  END IF;
+END $$;
 
